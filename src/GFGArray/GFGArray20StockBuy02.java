4db7c87ea46ea6f997ee26f05ab5d910424eff4c
belaletech <belalahmadswnbhr@gmail.com>
2024-07-11 02:18:51 +0530

    collection solution

---
+++ src/GFGArray/GFGArray20StockBuy02.java
@@ -0,0 +1,87 @@
+package GFGArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+Stock buy and sell
+Difficulty: MediumAccuracy: 29.18%Submissions: 268K+Points: 4
+The cost of stock on each day is given in an array A[] of size N. Find all the segments of days on which you buy and sell the stock such that the sum of difference between sell and buy prices is maximized. Each segment consists of indexes of two elements, first is index of day on which you buy stock and second is index of day on which you sell stock.
+Note: Since there can be multiple solutions, the driver code will print 1 if your answer is correct, otherwise, it will return 0. In case there's no profit the driver code will print the string "No Profit" for a correct solution.
+
+Example 1:
+
+Input:
+N = 7
+A[] = {100,180,260,310,40,535,695}
+Output:
+1
+Explanation:
+One possible solution is (0 3) (4 6)
+We can buy stock on day 0,
+and sell it on 3rd day, which will
+give us maximum profit. Now, we buy
+stock on day 4 and sell it on day 6.
+Example 2:
+
+Input:
+N = 5
+A[] = {4,2,2,2,4}
+Output:
+1
+Explanation:
+There are multiple possible solutions.
+one of them is (3 4)
+We can buy stock on day 3,
+and sell it on 4th day, which will
+give us maximum profit.
+ */
+public class GFGArray20StockBuy02 {
+    ArrayList<ArrayList<Integer>>stockBuySell(int A[],int n)
+    {
+        //your code
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        for(int i=1;i<n;i++)
+        {
+            if(A[i]>A[i-1])
+            {
+                list.add(new ArrayList<Integer>(Arrays.asList(i-1,i)));
+            }
+        }
+        return list;
+    }
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int A[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                A[i] = Integer.parseInt(inputLine[i]);
+            }
+            int p = 0;
+            for(int i=0; i<n-1; i++)
+                p += Math.max(0,A[i+1]-A[i]);
+
+            GFGArray20StockBuy02 obj = new GFGArray20StockBuy02();
+            ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
+            if(ans.size()==0)
+                System.out.print("No Profit");
+            else{
+                int c=0;
+                for(int i=0; i<ans.size(); i++)
+                    c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
+
+                if(c==p)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }System.out.println();
+        }
+    }
+}
